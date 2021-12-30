5047bc22e9824b4b3d1b89ae41fe3b0e106acfc8
AgustinSant <agustinmsant@gmail.com>
2021-12-30 16:32:47 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const path = require('path')
+const port = 3000
+const app = express()
+
+const productsModule = require('./utils/products')
+
+const viewsURL = path.join(__dirname, 'views')
+
+app.set('views', viewsURL)
+app.set('view engine', 'ejs')
+
+const publicURL = path.join(__dirname, 'public')
+
+app.use(express.static(publicURL))
+
+app.get('', (req, res) => {
+    productsModule.getAllProducts((err, products) => {
+        if(err) return res.status(500).send(err)
+        console.log(products)
+        return res.render('pages/index', { products })
+
+    })
+})
+
+app.get('/agregar', (req, res) => {
+    res.render('pages/agregar')
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Escuchando el puerto ${port}`)
+})
+
